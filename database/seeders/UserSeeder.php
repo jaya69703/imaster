73737b9618c36d40841cf70914bea81482c3749e
jaya69703 <jaya69703@gmail.com>
2023-08-24 11:13:23 +0700

    Nambahin default user Saya Bukan User

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,6 +16,16 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
+            'id' => '1',
+            'name' => 'Saya Bukan Admin',
+            'email' => 'sayabukanadmin@example.com',
+            'image' => 'default.png',
+            'phone' => '089612345678',
+            'password' => Hash::make('Namikaze1'),
+            'position_id' => '1',
+        ]);
+        DB::table('users')->insert([
+            'id' => '2',
             'name' => 'Super Administrator',
             'email' => 'superadmin@example.com',
             'image' => 'default.png',
